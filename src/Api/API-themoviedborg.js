0f76dfcbda306c89addcdb2d76a59c50d0dc3876
Maxximusan <dragonstonecave@gmail.com>
2023-09-16 09:37:49 +0300

    поменял ключ, прошлый выдавал ошибку

--- src/Api/API-themoviedborg.js
+++ src/Api/API-themoviedborg.js
@@ -1,15 +1,21 @@
 // Ключ API (v3 auth)
 // 03903974b6d688f171953b19c8d7c0eb
 
+// новый ключ от 15.09.23
+// 46ab2e1e7ed5243c494f829ac605818f
+
 // Пример API-запроса
 // https://api.themoviedb.org/3/movie/550?api_key=03903974b6d688f171953b19c8d7c0eb
 
 // Ключ доступа к API (v4 auth)
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzkwMzk3NGI2ZDY4OGYxNzE5NTNiMTljOGQ3YzBlYiIsInN1YiI6IjYzNzdkMzdiYzJmNDRiMDBkY2Q3ZTg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iK1N4LcxNyRlKxJiJcQuEsla-ZRwtOtyJQonyHTQpv8
 
+// новый ключ от 15.09.23
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NmFiMmUxZTdlZDUyNDNjNDk0ZjgyOWFjNjA1ODE4ZiIsInN1YiI6IjYzNzdkMzdiYzJmNDRiMDBkY2Q3ZTg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f5GYZQpJtDoIh_HQ4CFplzV6ivFHEHp3TEr4vwqP44o
+
 import axios from 'axios';
 
-const API_KEY = '03903974b6d688f171953b19c8d7c0eb';
+const API_KEY = '46ab2e1e7ed5243c494f829ac605818f';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const APIfetchTrandingFilms = () => {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,27 @@
-import { Routes, Route, } from 'react-router-dom';
-import { lazy } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
 
-import { Home } from '../pages/Home'
+import { Home } from '../pages/Home';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
-const Movies = lazy(() => import('pages/Movies'))
-const MovieDetails = lazy(() => import('pages/MovieDetails'))
-const NotFound = lazy(() => import('pages/NotFound'))
-const Cast = lazy(() => import('components/Cast/Cast'))
-const Reviews = lazy(() => import('components/Reviews/Reviews'))
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 export const App = () => {
   return (
-   
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-         <Route index element={<Home />} />
-         <Route path="/movies" element={<Movies />} />
-         <Route path="/movies/:movieId" element={<MovieDetails />}>
-           <Route path="cast" element={ <Cast />} />
-           <Route path="reviews" element={ <Reviews />} />
-         </Route>
-         <Route path="*" element={<NotFound />} />
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-      </Routes>
-    
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
-
